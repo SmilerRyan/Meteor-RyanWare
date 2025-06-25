44725b8f6e07ac3c6ae88612b2feb63aaefc5c78
SmilerRyan <ryanthornton03@gmail.com>
2025-06-25 10:20:18 +0100

    Update #build.bat

--- #build.bat
+++ #build.bat
@@ -1,1 +1,10 @@
-@cls & gradlew build & pause
+@echo off
+cls
+cd /d "%~dp0"
+cmd /c gradlew clean build
+
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd-HH-mm-ss"') do set "TS=%%i"
+
+copy /Y "%cd%\build\libs\meteor-RyanWare-0.1.jar" "%cd%\RyanWare-%TS%.jar"
+
+pause
