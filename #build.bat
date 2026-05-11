7a356f1ad8e908efac65f73174a9e716bcd40e88
SmilerRyan <ryanthornton03@gmail.com>
2026-05-11 05:10:57 +0100

    A

--- #build.bat
+++ #build.bat
@@ -16,8 +16,6 @@ cmd /c gradlew clean build --no-daemon
 
 for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd-HH-mm-ss"') do set "TS=%%i"
 
-::copy /Y "%cd%\build\libs\meteor-RyanWare-0.1.jar" "%cd%\RyanWare-%TS%.jar"
-
-move "%cd%\build\libs\meteor-RyanWare-0.1.jar" "%cd%\build\libs\RyanWare-%TS%.jar"
+copy /Y "%cd%\build\libs\meteor-RyanWare-0.1.jar" "%cd%\build\libs\RyanWare-%TS%.jar"
 
 if not defined NO_PAUSE pause
