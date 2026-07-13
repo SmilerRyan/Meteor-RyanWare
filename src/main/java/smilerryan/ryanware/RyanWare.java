de73e4eb47ab5cae4683a2930f6d8e6be4e87677
SmilerRyan <ryanthornton03@gmail.com>
2026-07-13 11:59:58 +0100

    + Tab Complete Privacy

--- src/main/java/smilerryan/ryanware/RyanWare.java
+++ src/main/java/smilerryan/ryanware/RyanWare.java
@@ -215,6 +215,7 @@ public class RyanWare extends MeteorAddon {
         Modules.get().add(new Screenshotter());
         Modules.get().add(new SoundBlocker());
         Modules.get().add(new SpeechToText());
+        Modules.get().add(new TabCompletePrivacy());
         Modules.get().add(new TabSortedByPing());
         Modules.get().add(new TntCleaner());
         Modules.get().add(new TotemAutoLeave());
