eaabe8c924743f5bd2bd72855a6922c5d0a0878a
SmilerRyan <ryanthornton03@gmail.com>
2026-07-16 13:12:24 +0100

    * AutoWalkForwards, add auto jump.

--- src/main/java/smilerryan/ryanware/modules_standard/automation/AutoWalkForwards.java
+++ src/main/java/smilerryan/ryanware/modules_standard/automation/AutoWalkForwards.java
@@ -1,11 +1,23 @@
 package smilerryan.ryanware.modules_standard.automation;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import smilerryan.ryanware.RyanWare;
 
 public class AutoWalkForwards extends Module {
+
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Boolean> autoJump = sgGeneral.add(new BoolSetting.Builder()
+        .name("auto-jump")
+        .description("Automatically holds the jump key.")
+        .defaultValue(false)
+        .build()
+    );
 
     public AutoWalkForwards() {
         super(RyanWare.CATEGORY_STANDARD, RyanWare.modulePrefix_standard + "Auto-Walk-Forwards", "Automatically presses forwardKey for you.");
@@ -15,12 +27,14 @@ public class AutoWalkForwards extends Module {
     public void onDeactivate() {
         if (mc.options == null) return;
         mc.options.forwardKey.setPressed(false);
+        if (autoJump.get()) mc.options.jumpKey.setPressed(false);
     }
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
         if (mc.player == null || mc.options == null) return;
         mc.options.forwardKey.setPressed(true);
+        if (autoJump.get()) mc.options.jumpKey.setPressed(true);
     }
 
 }
